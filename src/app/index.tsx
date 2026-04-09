4def1bd90080c152f280106826aed72edb640d7b
MrSpock182 <klebernunes182@gmail.com>
2026-04-09 14:58:20 -0300

    componente de botao

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,22 @@
 
 import { View, Text, StyleSheet } from 'react-native';
 
+import Button from '../components/button';
+
 export default function Index() {
     return (
         <View>
             <Text style={styles.title}>Hello World.</Text>
+            <Button 
+                title="Alterar"
+                onPress={() => alert('Clicou no botão')}
+                style={{ marginTop: 40 }}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     title: {
-        fontSize: 18
+        fontSize: 20
     }
 })
